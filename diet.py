6f03627940a71fd80abbe834b379af8fcb364f19
vexra <112993677+vexra@users.noreply.github.com>
2023-03-01 08:45:18 +0700

    Update diet.py

--- diet.py
+++ diet.py
@@ -2,4 +2,4 @@ age = int(input("Berapa umur kamu sekarang: "))
 
 res = (206.9 - (0.67 * age)) * 0.65
 
-print("target detak jantung kamu yang sedang diet:", )
+print("target detak jantung kamu yang sedang diet:", res)
